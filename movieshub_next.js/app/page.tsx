3fd8b497fa7605b448e0e2147ca5ab25dcec6596
Javier García <javigcpgr@gmail.com>
2023-10-01 11:52:46 +0200

    feat(loginPage)

---
+++ movieshub_next.js/app/page.tsx
@@ -0,0 +1,18 @@
+import styles from './page.module.css';
+
+export default function Login() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>
+        Movies<span className={styles.span}>H</span>ub
+      </h1>
+      <div className={styles.text}>
+        <p className={styles.p1}>Are you ready for the next level?</p>
+        <p>Dive in and start enjoying the ability to save all the movies you've already seen, ensuring you never forget what you've watched</p>
+      </div>
+      <div className={styles.button}>
+        <a href='/home' className={styles.login}>Log In</a>
+      </div>
+    </main>
+  )
+}
