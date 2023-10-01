d2a310d6a2bcbb522dd898bc87dd9f48119a094e
Javier García <javigcpgr@gmail.com>
2023-10-01 20:55:44 +0200

    fix(Home):grid template

--- movieshub_next.js/app/home/page.tsx
+++ movieshub_next.js/app/home/page.tsx
@@ -1,11 +1,17 @@
 import styles from './page.module.css';
 import React from 'react';
 import Nav from '../../components/nav/nav'
+import Card from '@/components/card/card';
 const Home = () => {
     return (
         <main className={styles.main}>
-            <p className={styles.logo}>H</p>
-            <Nav />
+            <section className={styles.section_header}>
+                <p className={styles.logo}>H</p>
+                <Nav />
+            </section>
+            <section className={styles.section_movies}>
+                <Card />
+            </section>
         </main>
     );
 };
---
+++ movieshub_next.js/components/card/card.tsx
@@ -0,0 +1,13 @@
+import styles from './card.module.css';
+import React from 'react'
+
+const Card = () => {
+    return (
+        <section>
+            <div className={styles.div_container}>
+
+            </div>
+        </section>
+    )
+}
+export default Card;
